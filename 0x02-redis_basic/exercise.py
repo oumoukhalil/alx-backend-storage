2b49ad11c07011d87f4215012dbdf1637c2c87af
oumoukhalil <fatsarr628@gmail.com>
2024-06-21 19:28:35 +0000

    0. Writing strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,6 +2,8 @@
 import uuid 
 import redis
 from typing import Optional, Callable, Union
+from functools import wraps
+
 
 class Cache :
     """class cach to to store cache"""
@@ -18,4 +20,26 @@ class Cache :
         key = str(uuid.uuid4())
         self._redis.set(key,data)
         return key
+
+    def get(self,key: str,fn: Optional [Callable] = None )-> Union [str,float,bytes, int,None]:
+        """methode to get data  by the key"""
+        data=self._redis.get(key)
+        if fn is not None:
+            data = fn(data)
+            return data
+        def get_str(self, key: str) -> str:
             
+       	 """get cache in string"""
+        data = self.get(key)
+        
+        return data.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """get cache in int"""
+        data = self.get(key)
+        try:
+            data = int(data.decode("utf-8"))
+        except Exception:
+            data = 0
+
+        return data
